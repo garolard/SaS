1b9a86701eea7ae5431084f41aace8b892b4f31c
Gabriel Ferreiro <gbril9119@outlook.com>
2015-02-17 21:40:43 +0100

    Cambiada implementación de guardado de texto para usar streams de WinRT en lugar de los de .NET

--- Service/DataService.cs
+++ Service/DataService.cs
@@ -45,23 +45,27 @@ namespace SaS.Service
             StorageFile targetFile =
                 await ApplicationData.Current.LocalFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
 
-            using (Stream stream = await targetFile.OpenStreamForWriteAsync())
-            {
-                StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
-                await writer.WriteAsync(text);
-                await writer.FlushAsync();
-            }
+            var inputStream = await targetFile.OpenAsync(FileAccessMode.ReadWrite);
+            var writeStream = inputStream.GetOutputStreamAt(0);
+
+            DataWriter writer = new DataWriter(writeStream);
+            writer.WriteString(text);
+            await writer.StoreAsync();
+            await writer.FlushAsync();
         }
 
         async public Task<string> ReadTextFromFile(string filename)
         {
             string fileText = String.Empty;
             StorageFile targetFile = await ApplicationData.Current.LocalFolder.GetFileAsync(filename);
-            using (IInputStream stream = await targetFile.OpenSequentialReadAsync())
-            {
-                StreamReader reader = new StreamReader(stream.AsStreamForRead());
-                fileText = await reader.ReadToEndAsync();
-            }
+
+            var inputStream = await targetFile.OpenAsync(FileAccessMode.Read);
+            var readStream = inputStream.GetInputStreamAt(0);
+
+            DataReader reader = new DataReader(readStream);
+            uint fileLength = await reader.LoadAsync((uint)inputStream.Size);
+            fileText = reader.ReadString(fileLength);
+
             return fileText;
         }
     }
