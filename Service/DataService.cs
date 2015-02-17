cfc1e92409bd2093b906b311ea40034833ead954
Gabriel Ferreiro <gbril9119@outlook.com>
2015-02-17 21:24:05 +0100

    Agregados métodos para leer/escribir texto desde/hacia un archivo en el almacenamiento local.

--- Service/DataService.cs
+++ Service/DataService.cs
@@ -7,6 +7,7 @@ using SaS.Service.Interface;
 using Windows.Storage;
 using System.IO;
 using System.Xml.Serialization;
+using Windows.Storage.Streams;
 
 namespace SaS.Service
 {
@@ -36,6 +37,32 @@ namespace SaS.Service
             }
 
             return targetObject;
+        }
+
+
+        async public Task SaveTextToFileAsync(string text, string filename)
+        {
+            StorageFile targetFile =
+                await ApplicationData.Current.LocalFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+
+            using (Stream stream = await targetFile.OpenStreamForWriteAsync())
+            {
+                StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+                await writer.WriteAsync(text);
+                await writer.FlushAsync();
+            }
+        }
+
+        async public Task<string> ReadTextFromFile(string filename)
+        {
+            string fileText = String.Empty;
+            StorageFile targetFile = await ApplicationData.Current.LocalFolder.GetFileAsync(filename);
+            using (IInputStream stream = await targetFile.OpenSequentialReadAsync())
+            {
+                StreamReader reader = new StreamReader(stream.AsStreamForRead());
+                fileText = await reader.ReadToEndAsync();
+            }
+            return fileText;
         }
     }
 }
