291b4acd26c273efb03ed5b3bf7d973e0b45032d
Gabriel Ferreiro <gbril9119@outlook.com>
2015-03-07 12:47:11 +0100

    Agregadas clases muy comunes usadas en las plantillas de VisualStudio

---
+++ Common/BasePage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+using SaS.Common;
+
+namespace SaS.Common
+{
+    public class BasePage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private BaseViewModel viewModel;
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Gets the view model for this <see cref="Page"/>.
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        public BasePage()
+        {
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+        }
+
+        protected virtual void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+        protected virtual void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        protected override void OnNavigatedTo(Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            this.navigationHelper.OnNavigatedTo(e);
+
+            viewModel = (BaseViewModel)this.DataContext;
+            viewModel.SetAppFrame(this.Frame);
+            viewModel.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            this.navigationHelper.OnNavigatedFrom(e);
+            viewModel.OnNavigatedFrom(e);
+        }
+    }
+}
---
+++ Common/BaseViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace SaS.Common
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        private Frame appFrame;
+        private bool isBusy;
+
+        public BaseViewModel()
+        {
+        }
+
+        public Frame AppFrame
+        {
+            get { return appFrame; }
+        }
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                if (value != isBusy)
+                {
+                    isBusy = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public abstract Task OnNavigatedFrom(NavigationEventArgs args);
+        public abstract Task OnNavigatingFrom(NavigatingCancelEventArgs args);
+        public abstract Task OnNavigatedTo(NavigationEventArgs args);
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        internal void SetAppFrame(Frame viewFrame)
+        {
+            appFrame = viewFrame;
+        }
+    }
+}
